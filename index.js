0b45af5d8b7592d63b75e400c8b7941e68172b22
Girish Parate <girishparate41@gmail.com>
2022-01-16 14:35:13 +0530

    Android codepush added

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import { App } from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+import codePush from "react-native-code-push";
+
+AppRegistry.registerComponent(appName, () => codePush(App));
